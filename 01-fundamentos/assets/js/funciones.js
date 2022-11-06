4881db064449c92b0b9cbf5f9f8c1cfc5d9c5c3c
Enoc Castro Vargas <enocco70@gmail.com>
2022-11-06 17:24:53 -0600

    Retorno de las funciones

--- 01-fundamentos/assets/js/funciones.js
+++ 01-fundamentos/assets/js/funciones.js
@@ -1,7 +1,11 @@
 
 function saludar( nombre ) {
-  console.log( arguments )
-  console.log('Hola ' + nombre );
+  // console.log( arguments )
+  // console.log('Hola ' + nombre );
+  return [1, 2];
+
+  //! Esto nunca se va a ejecutar
+  console.log('Soy un código que está después del return');
 }
 
 const saludar2 = function( nombre ) {
@@ -18,8 +22,29 @@ const saludarFlecha2 = ( nombre ) => {
 
 
 
-saludar( 'Enoc', 40, true, 'Costa Rica' );
+const retornoDeSaludar = saludar( 'Enoc', 40, true, 'Costa Rica' ); // 1
+
 // saludar2( 'Enoc' );
 
-saludarFlecha();
-saludarFlecha2('Lionel');
+// saludarFlecha();
+// saludarFlecha2('Lionel');
+
+function sumar( a, b ) {
+  return a + b;
+}
+
+// const sumar2 = ( a, b ) => {
+//   return a + b;
+// }
+const sumar2 = ( a, b ) => a + b;
+
+function getAleatorio(){
+  return Math.random();
+}
+
+// En una función de flecha que no tenga llaves { }
+const getAleatorio2 = () => Math.random();
+
+
+
+console.log( getAleatorio2() );
