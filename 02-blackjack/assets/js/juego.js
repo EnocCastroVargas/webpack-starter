11841820f6b4554ec082ad998ec192273c544bd7
Enoc Castro Vargas <enocco70@gmail.com>
2022-11-07 18:24:38 -0600

    Crear cartas en el HTML

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -14,6 +14,7 @@ let puntosJugador = 0,
 
 // Referencias del HTML
 const btnPedir = document.querySelector('#btnPedir');
+const divCartasJugador = document.querySelector('#jugador-cartas');
 const puntosHTML = document.querySelectorAll('small');
 
 
@@ -68,10 +69,21 @@ btnPedir.addEventListener('click', () => {
 
   puntosJugador = puntosJugador + valorCarta( carta );
   puntosHTML[0].innerText = puntosJugador;
- 
   
+  // <img class="carta" src="assets/cartas/10C.png">
+  const imgCarta = document.createElement('img');
+  imgCarta.src = `assets/cartas/${ carta }.png`;
+  imgCarta.classList.add('carta');
+  divCartasJugador.append( imgCarta );
+
+  if ( puntosJugador > 21 ) {
+    console.warn('Lo siento mucho, perdiste');
+    btnPedir.disabled = true;
+  } else if ( puntosJugador === 21 ) {
+    console.warn('21, genial!');
+    btnPedir.disabled = true;
+  }
+
 
 });
-
-
 
