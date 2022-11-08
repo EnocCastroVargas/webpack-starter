5db559ddebf50585916ee3c9e31197474ad3ccdd
Enoc Castro Vargas <enocco70@gmail.com>
2022-11-07 20:19:50 -0600

    Turno de la computadora

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -14,7 +14,11 @@ let puntosJugador = 0,
 
 // Referencias del HTML
 const btnPedir = document.querySelector('#btnPedir');
-const divCartasJugador = document.querySelector('#jugador-cartas');
+const btnDetener = document.querySelector('#btnDetener');
+
+const divCartasJugador     = document.querySelector('#jugador-cartas');
+const divCartasComputadora = document.querySelector('#computadora-cartas');
+
 const puntosHTML = document.querySelectorAll('small');
 
 
@@ -62,6 +66,31 @@ const valorCarta = ( carta ) => {
          : valor * 1;
 }
 
+// Turno de la computadora
+const turnoComputadora = ( puntosMinimos ) => {
+  
+  do {
+    
+    const carta = pedirCarta();
+
+    puntosComputadora = puntosComputadora + valorCarta( carta );
+    puntosHTML[1].innerText = puntosComputadora;
+    
+    // <img class="carta" src="assets/cartas/10C.png">
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/cartas/${ carta }.png`; // 3H, JD
+    imgCarta.classList.add('carta');
+    divCartasComputadora.append( imgCarta );
+
+    if ( puntosMinimos > 21 ) {
+      break;
+    }
+
+  } while ( (puntosComputadora < puntosMinimos) && ( puntosMinimos <= 21 ) );
+
+}
+
+
 // Eventos
 btnPedir.addEventListener('click', () => {
 
@@ -79,11 +108,20 @@ btnPedir.addEventListener('click', () => {
   if ( puntosJugador > 21 ) {
     console.warn('Lo siento mucho, perdiste');
     btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoComputadora( puntosJugador );
+
   } else if ( puntosJugador === 21 ) {
     console.warn('21, genial!');
     btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoComputadora( puntosJugador );
   }
+});
 
-
+btnDetener.addEventListener('click', () => {
+  btnPedir.disabled = true;
+  btnDetener.disabled = true;
+  turnoComputadora( puntosJugador );
 });
 
