bd0a1f1225513f4f7e53abd6fff122c9f80e0345
Enoc Castro Vargas <enocco70@gmail.com>
2022-11-11 17:05:42 -0600

    Métodos en las clases

--- 01-fundamentos/assets/js/classes/class.js
+++ 01-fundamentos/assets/js/classes/class.js
@@ -12,13 +12,25 @@ class Persona {
     this.frase  = frase;
   }
 
+  quienSoy() {
+    console.log(`Soy ${ this.nombre } y mi identidad es ${ this.codigo }`);
+  }
+
+  miFrase() {
+    this.quienSoy();
+    console.log(`${ this.codigo } dice: ${ this.frase }`)
+  }
+
 }
 
 
 
-const spiderman = new Persona( 'Peter Parker', 'Spider', 'Soy tu amigable vecino Spiderman' );
+const spiderman = new Persona( 'Peter Parker', 'Spiderman', 'Soy tu amigable vecino Spiderman' );
 const ironman   = new Persona( 'Tony Stark', 'Ironman', 'Yo soy Ironman' );
 console.log( spiderman );
 console.log( ironman );
+
+spiderman.miFrase();
+ironman.miFrase();
 
 
