f8da7a87821eff08f9f0c898aa6632b6c2e2e2b2
Enoc Castro Vargas <enocco70@gmail.com>
2022-11-11 17:26:53 -0600

    Sets y Gets

--- 01-fundamentos/assets/js/classes/class.js
+++ 01-fundamentos/assets/js/classes/class.js
@@ -1,15 +1,23 @@
 
 
 class Persona {
-  
   nombre = '';
   codigo = '';
   frase  = '';
+  comida = '';
+
   constructor( nombre = 'Sin nombre', codigo = 'Sin código', frase = 'Sin frase' ) {
     
     this.codigo = codigo;
     this.nombre = nombre;
     this.frase  = frase;
+  }
+
+  set setComidaFavorita( comida ) {
+    this.comida = comida.toUpperCase();
+  }
+  get getComidaFavorita() {
+    return `La comida de favorita de ${ this.nombre} es ${ this.comida }`;
   }
 
   quienSoy() {
@@ -27,10 +35,15 @@ class Persona {
 
 const spiderman = new Persona( 'Peter Parker', 'Spiderman', 'Soy tu amigable vecino Spiderman' );
 const ironman   = new Persona( 'Tony Stark', 'Ironman', 'Yo soy Ironman' );
-console.log( spiderman );
-console.log( ironman );
+// console.log( ironman );
 
 spiderman.miFrase();
-ironman.miFrase();
+// ironman.miFrase();
+
+spiderman.setComidaFavorita = 'El pie de cereza de la tía May';
+// spiderman.comida = 'Duende Verde';
 
 
+console.log( spiderman.getComidaFavorita );
+
+console.log( spiderman );
